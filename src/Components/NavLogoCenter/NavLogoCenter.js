9b5b88fdd8bcbd39403b47009d4fa69a5d61b9d2
Mike Morris <michaelmorrisg@gmail.com>
2019-08-28 22:11:09 -0600

    initial commit

---
+++ src/Components/NavLogoCenter/NavLogoCenter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavLogoCenter extends Component {
+
+    render() {
+        return (
+            <div className="nav-main">
+                Hey there
+            </div>
+        )
+    }
+}
+
+export default NavLogoCenter;
